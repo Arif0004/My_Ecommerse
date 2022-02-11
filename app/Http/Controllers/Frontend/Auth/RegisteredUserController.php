9aebb531875ac012a7244e6a0c9342171069e5fa
Arif0004 <arifmahmudaj6@gmail.com>
2022-02-11 22:27:33 +0600

    ecommerse

---
+++ app/Http/Controllers/Frontend/Auth/RegisteredUserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Models\Customer;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        // return 1;
+        return view('Frontend.auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        // return $request->date_of_birth;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:customers'],
+            'password' => ['required', 'confirmed'],
+            'date_of_birth' => ['required', 'before_or_equal:today'],
+        ]);
+
+        $user = Customer::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'date_of_birth' => $request->date_of_birth
+        ]);
+
+        //  event(new Registered($user));
+
+        Auth::guard('customer')->login($user);
+
+        return redirect(RouteServiceProvider::CUSTOMER_HOME);
+    }
+}
